3ae9b84efb9472daa6fa2f80e022206fa1712992
Katya <konevakatya639@gmail.com>
2026-06-09 14:51:14 +0500

    Flask добавлен

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения из файла .env
+load_dotenv()
+
+app = Flask(__name__)
+# Секретный ключ нужен для шифрования сессий и защиты форм (понадобится в Часе 3 и 4)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+@app.route('/')
+def home():
+    return "<h1>Привет, Веб! Это мой журнал успеваемости.</h1>"
+
+
+@app.route('/health')
+def health_check():
+    return "<h1>Приложение работает</h1>"
+
+if __name__ == '__main__':
+    # debug=True автоматически перезагружает сервер при изменении кода
+    app.run(debug=True)
+
